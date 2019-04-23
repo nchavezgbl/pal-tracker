d88dc779fa6ac19801c7ce114ea55c0da24ff2b4
pal_user <nchavez@gblsys.com>
2019-04-23 10:51:31 -0700

    testing

--- src/test/java/test/pivotal/pal/trackerapi/WelcomeApiTest.java
+++ src/test/java/test/pivotal/pal/trackerapi/WelcomeApiTest.java
@@ -14,7 +14,7 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = PalTrackerApplication.class, webEnvironment = RANDOM_PORT)
 public class WelcomeApiTest {
-
+    //comment
     @Autowired
     private TestRestTemplate restTemplate;
 
