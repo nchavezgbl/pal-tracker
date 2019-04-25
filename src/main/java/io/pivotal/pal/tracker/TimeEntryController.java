03def261486a15edc0eae3490b1c2b3c1857e84b
pal_user <nchavez@gblsys.com>
2019-04-25 14:23:04 -0700

    health lab

--- src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
@@ -1,5 +1,8 @@
 package io.pivotal.pal.tracker;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.DistributionSummary;
+import io.micrometer.core.instrument.MeterRegistry;
 import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,20 +19,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/time-entries")
 public class TimeEntryController {
   private final TimeEntryRepository timeEntryRepository;
+  private final DistributionSummary timeEntrySummary;
+  private final Counter actionCounter;
 
-  public TimeEntryController(TimeEntryRepository timeEntryRepository) {
+  public TimeEntryController(TimeEntryRepository timeEntryRepository, MeterRegistry meterRegistry) {
     this.timeEntryRepository = timeEntryRepository;
+    timeEntrySummary = meterRegistry.summary("timeEntry.summary");
+    actionCounter = meterRegistry.counter("timeEntry.actionCounter");
   }
   @PostMapping
   //return a ReponseEntity with a newly created TimeEntry and the HTTPS status
   public ResponseEntity create(@RequestBody TimeEntry timeEntryToCreate) {
     TimeEntry timeEntry = timeEntryRepository.create(timeEntryToCreate);
-
+    actionCounter.increment();
+    timeEntrySummary.record(timeEntryRepository.list().size());
     return new ResponseEntity<TimeEntry>(timeEntry, HttpStatus.CREATED);
   }
   @GetMapping("/{timeEntryId}")
   public ResponseEntity<TimeEntry> read(@PathVariable long timeEntryId) {
     TimeEntry timeEntry = timeEntryRepository.find(timeEntryId);
+    actionCounter.increment();
     if(timeEntry != null){
       return new ResponseEntity<TimeEntry>(timeEntry, HttpStatus.OK);
     }else{
@@ -40,12 +49,14 @@ public class TimeEntryController {
   @GetMapping
   public ResponseEntity<List<TimeEntry>> list() {
     List<TimeEntry> timeEntryList = timeEntryRepository.list();
+    actionCounter.increment();
     return new ResponseEntity<List<TimeEntry>>(timeEntryList, HttpStatus.OK);
   }
 
   @PutMapping("/{timeEntryId}")
   public ResponseEntity update(@PathVariable long timeEntryId, @RequestBody TimeEntry expected) {
     TimeEntry updatedTimeEntry = timeEntryRepository.update(timeEntryId, expected);
+    actionCounter.increment();
     if(updatedTimeEntry != null){
       return new ResponseEntity<TimeEntry>(updatedTimeEntry, HttpStatus.OK);
     }else{
@@ -55,6 +66,8 @@ public class TimeEntryController {
   @DeleteMapping("/{timeEntryId}")
   public ResponseEntity<TimeEntry> delete(@PathVariable long timeEntryId) {
     timeEntryRepository.delete(timeEntryId);
+    actionCounter.increment();
+    timeEntrySummary.record(timeEntryRepository.list().size());
     return new ResponseEntity<TimeEntry>(HttpStatus.NO_CONTENT);
   }
 }
